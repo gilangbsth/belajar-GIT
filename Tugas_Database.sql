fa6d548af9662c63b7022946f8e0f9470bc65467
gilangbsth <gilang.basith@gmail.com>
2022-11-23 17:39:11 +0700

    [Add Tugas]

---
+++ Tugas_Database.sql
@@ -0,0 +1,9 @@
+select*from public.tbl_users
+
+--insert into tbl_users (id, username, email, password, address)
+--values ('04','user345', 'user555@mail.com', '54321', 'Pekanbaru')
+
+--delete from tbl_users where id=3
+
+--update tbl_users
+--set email='user321@ymail.com' where username= 'user2';
